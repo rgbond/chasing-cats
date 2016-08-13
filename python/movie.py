4f99e57cf5963a1964d438fb473accd04e96d806
Bob Bond <r.bond@frontier.com>
2016-08-13 16:28:46 -0700

    Add -t option and 'p' key

--- python/movie.py
+++ python/movie.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-# Usage: movie.py f1.jpg f2.jpg f3.png
+# Usage: [-t ms] movie file_list
 # Shows a bunch of .png or .jpg files at 1/3 second intervals
 # Handy for going through a bunch of images quickly
+# -t - set delay between frames.
+#
 
 import sys
 import os.path
@@ -15,12 +17,20 @@ cv2.moveWindow("movie", 300, 300)
 if len(sys.argv) == 1:
     exit(0)
 
-for fn in sys.argv[1:]:
+t = 330
+ai = 1
+if sys.argv[1] == "-t":
+    t = int(sys.argv[2])
+    ai += 2
+
+for fn in sys.argv[ai:]:
     m = cv2.imread(fn)
     if m == None:
         continue
     cv2.imshow('movie', m);
-    key = cv2.waitKey(330)
+    key = cv2.waitKey(t)
     key = key & 0xff
     if key == 27 or key == ord('q'):
         exit(0)
+    elif key == ord('p'):
+        print fn
